46b227bb257c3ce816b40aad0748884a375d3472
zx026 <yb131567@gmail.com>
2025-11-29 17:59:36 +0530

    Update sudoers.py

--- BABYMUSIC/plugins/sudo/sudoers.py
+++ BABYMUSIC/plugins/sudo/sudoers.py
@@ -61,7 +61,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("❍ ᴜᴛᴛᴀᴍ ᴋᴀ ʟᴜɴᴅ ᴅᴇᴋʜᴇɢᴀ ᴀᴘɴɪ ʙᴇʜᴀɴ ᴋᴏ ᴅᴇ ᴅᴇɴᴀ 🤪 ●", show_alert=True)
+        return await callback_query.answer("❍ Rudra ᴋᴀ ʟᴜɴᴅ ᴅᴇᴋʜᴇɢᴀ ᴀᴘɴɪ ʙᴇʜᴀɴ ᴋᴏ ᴅᴇ ᴅᴇɴᴀ 🤪 ●", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
